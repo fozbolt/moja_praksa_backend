3b7c164bab5dedb59976883dd6c6255901928762
fozbolt <58041469+fozbolt@users.noreply.github.com>
2020-09-02 08:51:36 +0200

    message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import auth from './auth.js'
 
 
 const app = express() // instanciranje aplikacije
-const port = 3000 // port na kojem će web server slušati
+const port = process.env.PORT // port na kojem će web server slušati
 
 app.use(cors())
 
